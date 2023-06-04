210a96112307973043a43283cd935bc4a24e47e6
Senan Memmedov <senanm260@gmail.com>
2023-06-04 20:26:31 +0400

    add filter

--- js/app.js
+++ js/app.js
@@ -30,15 +30,14 @@ function PrevButton() {
   createInterval();
 }
 
-const sliderContent = document.querySelector(".slider-main")
+const sliderContent = document.querySelector(".slider-main");
 
 function Next() {
   slideContent.forEach((item, index) => {
     if (index == counter) {
-      sliderContent.style.transform = "translateX(0%)"
+      sliderContent.style.transform = "translateX(0%)";
     } else {
-      sliderContent.style.transform = "translateX(-50%)"
-
+      sliderContent.style.transform = "translateX(-50%)";
     }
   });
 }
@@ -46,9 +45,9 @@ function Next() {
 function Prev() {
   slideContent.forEach((item, index) => {
     if (index == counter) {
-      sliderContent.style.transform = "translateX(-50%)"
+      sliderContent.style.transform = "translateX(-50%)";
     } else {
-      sliderContent.style.transform = "translateX(0%)"
+      sliderContent.style.transform = "translateX(0%)";
     }
   });
 }
@@ -65,50 +64,63 @@ window.addEventListener("DOMContentLoaded", () => {
   createInterval();
 });
 
-// Define Pop up 
-const techHover = document.querySelector(".tech")
-const techPop = document.querySelector(".pop2")
-const buisnesHover = document.querySelector(".buisnes")
-const buisnesPopUp = document.querySelector(".right-popUP")
-const popupHover = document.querySelector(".category")
-const openPopUp = document.querySelector(".category-display-pop-up")
+// Define Pop up
+const techHover = document.querySelector(".tech");
+const techPop = document.querySelector(".pop2");
+const buisnesHover = document.querySelector(".buisnes");
+const buisnesPopUp = document.querySelector(".right-popUP");
+const popupHover = document.querySelector(".category");
+const openPopUp = document.querySelector(".category-display-pop-up");
 
 // Right pop
-techHover.addEventListener("mouseenter",() =>{
-  techPop.style.scale = "1"
-  techPop.style.display = "block"
-})
-techHover.addEventListener("mouseleave",() =>{
-  techPop.style.scale = "0.95"
-  techPop.style.display = "none"
-})
-techPop.addEventListener("mouseleave",()=>{
-  techPop.style.scale = "0.95"
-  techPop.style.display = "none"
-})
+techHover.addEventListener("mouseenter", () => {
+  techPop.style.scale = "1";
+  techPop.style.display = "block";
+});
+techHover.addEventListener("mouseleave", () => {
+  techPop.style.scale = "0.95";
+  techPop.style.display = "none";
+});
+techPop.addEventListener("mouseleave", () => {
+  techPop.style.scale = "0.95";
+  techPop.style.display = "none";
+});
 
 // left pop
-buisnesHover.addEventListener("mouseenter",() =>{
-  buisnesPopUp.style.display = "block"
-})
-buisnesHover.addEventListener("mouseleave",() =>{
-  techPop.style.scale = "0.95"
-  buisnesPopUp.style.display = "none"
-})
-buisnesPopUp.addEventListener("mouseleave",()=>{
-  techPop.style.scale = "0.95"
-  buisnesPopUp.style.display = "none"
-})
+buisnesHover.addEventListener("mouseenter", () => {
+  buisnesPopUp.style.display = "block";
+});
+buisnesHover.addEventListener("mouseleave", () => {
+  techPop.style.scale = "0.95";
+  buisnesPopUp.style.display = "none";
+});
+buisnesPopUp.addEventListener("mouseleave", () => {
+  techPop.style.scale = "0.95";
+  buisnesPopUp.style.display = "none";
+});
 
 // Menu pop
-popupHover.addEventListener("mouseenter",() =>{
-  openPopUp.style.display = "block"
-})
-popupHover.addEventListener("mouseleave",() =>{
-  techPop.style.scale = "0.95"
-  openPopUp.style.display = "none"
-})
-openPopUp.addEventListener("mouseleave",()=>{
-  techPop.style.scale = "0.95"
-  openPopUp.style.display = "none"
-})
+popupHover.addEventListener("mouseenter", () => {
+  openPopUp.style.display = "block";
+});
+popupHover.addEventListener("mouseleave", () => {
+  techPop.style.scale = "0.95";
+  openPopUp.style.display = "none";
+});
+openPopUp.addEventListener("mouseleave", () => {
+  techPop.style.scale = "0.95";
+  openPopUp.style.display = "none";
+});
+
+// Define shop cart
+const shop = document.querySelector(".shop");
+const shopcart = document.querySelector(".shop-cart");
+shop.addEventListener("mouseenter", () => {
+  shopcart.style.display = "flex";
+});
+shop.addEventListener("mouseleave", () => {
+  shopcart.style.display = "none";
+});
+openPopUp.addEventListener("mouseleave", () => {
+  shopcart.style.display = "none";
+});
---
+++ js/data.js
@@ -0,0 +1,71 @@
+let Filterdatas = [
+  {
+    id: "python",
+    "head text": "Expand your career opportunities with Python",
+    "paragraph text":
+      "Take one of Udemy’s range of Python courses and learn how to code using this incredibly useful language. Its simple syntax and readability makes Python perfect for Flask, Django, data science, and machine learning. You’ll learn how to build everything from games to sites to apps. Choose from a range of courses that will appeal to",
+    "button text": "Python",
+  },
+  {
+    id: "excel",
+    "head text": "Analyze and visualize data with Excel",
+    "paragraph text":
+      "Take a Microsoft Excel course from Udemy, and learn how to use this industry-standard software. Real-world experts will show you the basics like how to organize data into sheets, rows and columns, and advanced techniques like creating complex dynamic formulas. Both small businesses and large companies use Excel to",
+    "button text": "Excel",
+  },
+  {
+    id: "webdev",
+    "head text": "Build websites and applications with Web Development",
+    "paragraph text":
+      "The world of web development is as wide as the internet itself. Much of our social and vocational lives play out on the internet, which prompts new industries aimed at creating, managing, and debugging the websites and applications that we increasingly rely on.",
+    "button text": "Web Development",
+  },
+  {
+    id: "js",
+    "head text": "Grow your software development skills with JavaScript",
+    "paragraph text":
+      "JavaScript is a text-based computer programming language used to make dynamic web pages. A must-learn for aspiring web developers or programmers, JavaScript can be used for features like image carousels, displaying countdowns and timers, and playing media on a webpage. With JavaScript online classes, you can learn to build",
+    "button text": "JavaScript",
+  },
+  {
+    id: "data",
+    "head text": "Lead data-driven decisions with Data Science",
+    "paragraph text":
+      "Data science application is an in-demand skill in many industries worldwide — including finance, transportation, education, manufacturing, human resources, and banking. Explore data science courses with Python, statistics, machine learning, and more to grow your knowledge. Get data science training if you’re into research, statistics,",
+    "button text": "Data Sience",
+  },
+  {
+    id: "amazon",
+    "head text": "Become an expert in cloud computing with AWS Certification",
+    "paragraph text":
+      "Amazon Web Services (AWS) is a cloud computing platform with more than 200 featured services. Whether or not you aim for certification, an AWS course offers the theory and practical skills you need to land a job in cloud development, sales, engineering, networking, and more. The better you become at cloud computing, the more",
+    "button text": "Amazon AWS",
+  },
+  {
+    id: "draw",
+    "head text": "Expand your creative skillset with Drawing",
+    "paragraph text":
+      "Want to start drawing for fun or take your craft to the next level? Explore our online drawing classes and learn pencil drawing, figure drawing, cartoon drawing, character drawing for cartoons and anime, illustration, sketching, shading and more. Take an overview course on the fundamentals of drawing or zero in on an area",
+    "button text": "Drawing",
+  },
+];
+
+const headTEXT = document.querySelector(".headtext")
+const parTEXT = document.querySelector(".ptext")
+const buttTEXT = document.querySelector(".atext")
+
+
+const button = document.querySelectorAll(".filter-wrapper button")
+
+
+button.forEach((buttons,index) =>{
+    buttons.addEventListener("click",()=>{
+        if(buttons.id == Filterdatas[index].id){
+            headTEXT.innerHTML = Filterdatas[index]["head text"]
+            parTEXT.innerHTML = Filterdatas[index]["paragraph text"]
+            buttTEXT.innerHTML = "Explore "+Filterdatas[index]['button text']
+        }
+    })
+
+
+})
