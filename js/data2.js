fa34f36bdc8d4f3d4dc90447adc745cd319ac1f7
Senan Memmedov <senanm260@gmail.com>
2023-06-05 21:12:03 +0400

    data update

--- js/data2.js
+++ js/data2.js
@@ -89,6 +89,96 @@ const userData = [
     price: "64.99",
     bestseller: false,
   },
+  {
+    id: 5,
+    category: "python",
+    img: "https://img-b.udemycdn.com/course/240x135/903744_8eb2.jpg",
+    headTexts: "Python for Data Science and Machine Learning Bootcamp",
+    user: "Jose Portilla",
+    rating: "4.6",
+    starImg: `
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/halfStar.png">
+        `,
+    peopleNumber: "(131,362)",
+    price: "124.99",
+    bestseller: false,
+  },
+  {
+    id: 6,
+    category: "python",
+    img: "https://img-b.udemycdn.com/course/240x135/629302_8a2d_2.jpg",
+    headTexts: "Learn Python Programming Masterclass",
+    user: "Tim Buchalka",
+    rating: "4.6",
+    starImg: `
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/halfStar.png">
+        `,
+    peopleNumber: "(98,731)",
+    price: "89.99",
+    bestseller: false,
+  },
+  {
+    id: 7,
+    category: "python",
+    img: "https://img-b.udemycdn.com/course/240x135/3047216_4888_2.jpg",
+    headTexts: "Python for beginners - Learn all the basics of python",
+    user: "Yassin Marco",
+    rating: "4.3",
+    starImg: `
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/halfStar.png">
+        `,
+    peopleNumber: "(98,731)",
+    price: "54.99",
+    bestseller: false,
+  },
+  {
+    id: 8,
+    category: "python",
+    img: "https://img-b.udemycdn.com/course/240x135/1495788_1aaa_2.jpg",
+    headTexts: "Python for beginners - Learn all the basics of python",
+    user: "Ivan Gomes",
+    rating: "4.5",
+    starImg: `
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/halfStar.png">
+        `,
+    peopleNumber: "(12,004)",
+    price: "69.99",
+    bestseller: false,
+  },
+  {
+    id: 9,
+    category: "python",
+    img: "https://img-b.udemycdn.com/course/240x135/1554180_150a_2.jpg",
+    headTexts: "Python Network Programming for Network Engineers (Python 3)",
+    user: "David Bombal",
+    rating: "4.8",
+    starImg: `
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/FullStar.png">
+        <img src="img/star/halfStar.png">
+        `,
+    peopleNumber: "(8,169)",
+    price: "89.99",
+    bestseller: false,
+  },
 ];
 
 const mainHTML = document.querySelector(".card-slide-wrapper");
